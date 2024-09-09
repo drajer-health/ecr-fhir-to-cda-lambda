4a0b5ef104b39202e06e854d70667c141a75af5b
Girish <bgirishrao@drajer.com>
2024-09-09 15:44:35 -0400

    removed SQS

--- src/main/java/com/drajer/ecr/fhir2cda/converter/FHIR2CDAConverterLambdaFunctionHandler.java
+++ src/main/java/com/drajer/ecr/fhir2cda/converter/FHIR2CDAConverterLambdaFunctionHandler.java
@@ -61,17 +61,16 @@ public class FHIR2CDAConverterLambdaFunctionHandler implements RequestHandler<S3
 	}	
 	
 	public FHIR2CDAConverterLambdaFunctionHandler() throws IOException {
-		String bucketName = System.getenv("SQS_NAME");
+		String bucketName = System.getenv("BUCKET_NAME");
 		if (bucketName == null || bucketName.isEmpty()) {
-			throw new IllegalArgumentException("SQS name is not set in the environment variables.");
+			throw new IllegalArgumentException("S3 bucket name is not set in the environment variables.");
 		}		
 		// Load the Saxon processor and transformer
-		this.processor = createSaxonProcessor();
+		this.processor = createSaxonProcessor(bucketName);
 		this.transformer = initializeTransformer();
 	}	
 	
-	private Processor createSaxonProcessor() throws IOException {
-		String bucketName = System.getenv("BUCKET_NAME");
+	private Processor createSaxonProcessor(String bucketName) throws IOException {
 		String licenseFilePath = "/tmp/saxon-license.lic"; // Ensure temp path is used
 		ProfessionalConfiguration configuration = new ProfessionalConfiguration();
 		String key = "license/saxon-license.lic";
